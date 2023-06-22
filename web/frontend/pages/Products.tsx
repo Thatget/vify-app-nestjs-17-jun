2875b039a7f80611c378ad07b20b4873970c0aa4
brucenguyen86 <130854166+brucenguyen86@users.noreply.github.com>
2023-06-22 13:19:30 +0700

    getting started component and quotes with mock data

--- web/frontend/pages/Products.tsx
+++ web/frontend/pages/Products.tsx
@@ -1,23 +1,6 @@
-import React from "react";
-import { Page, Layout ,LegacyCard,
-    LegacyStack,
-    Select,
-    TextField,
-    Button,
-    Form} from "@shopify/polaris"
-
 export default function Products() {
     return (
-        <Page>
-        <Layout sectioned={true}>
-            <Layout.Section>
-                <LegacyCard>
-                    abc
-                </LegacyCard>
-
-            </Layout.Section>
-
-        </Layout>
-        </Page>
+        <h1> Setting </h1>
     )
+
 }
