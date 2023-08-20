ff80153f73f8b1561d4f052d022e68c7f0050698
Lan kha <quyet.nguyen.7712826@gmail.com>
2023-08-21 04:26:47 +0700

    change product in quote to json

--- web/backend/src/modules/quote/quote-analysis.service.ts
+++ web/backend/src/modules/quote/quote-analysis.service.ts
@@ -8,11 +8,18 @@ export class QuoteAnalysisService {
     @Inject('QUOTE_REPOSITORY')
     private readonly quoteRepository: Repository<Quote>,
   ) {}
-  async countProduct(store_id: number) {
+  async countProduct(store_id: number, limit = 10) {
     const a = await this.quoteRepository
       .createQueryBuilder('quote')
       .select('JSON_UNQUOTE(JSON_EXTRACT(quote.product, "$.selected_product.id"))', 'productId')
+      .addSelect('JSON_UNQUOTE(JSON_EXTRACT(quote.product, "$.selected_product.title"))', 'productTitle')
+      .addSelect('JSON_UNQUOTE(JSON_EXTRACT(quote.product, "$.selected_product.image"))', 'productImage')
+      .addSelect('JSON_UNQUOTE(JSON_EXTRACT(quote.product, "$.selected_variant.id"))', 'variantId')
+      .addSelect('JSON_UNQUOTE(JSON_EXTRACT(quote.product, "$.selected_variant.title"))', 'variantTitle')
+      .where({store_id: 22})
+      .limit(limit)
+      .groupBy('variantId')
       .getRawMany()
-    console.log(a);
+      console.log(a)
   }
 }
